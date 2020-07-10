5045e3d391b830c93abda121ddd0fdff079c4924
Alejandro <alejandroacho@hotmail.com>
2020-07-10 12:33:08 +0200

    init file to /tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.fixture
 def client():
     app = webserver()
-    return app.test_client()
+    return app.test_client()    
  
 def test_index_pages_works(client):
     response = client.get('/')
