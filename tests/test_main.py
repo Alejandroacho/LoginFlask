771f88502f519609b3573c330e068cbe855fe3d4
Alejandro <alejandroacho@hotmail.com>
2020-07-10 14:58:07 +0200

    Requirements text and tests fixed

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,3 +9,7 @@ def client():
 def test_index_pages_works(client):
     response = client.get('/')
     assert response.status_code==200
+
+#def test_index_pages_works(client):
+#    response = client.get('mailto:contact@neveralone.com')
+#    assert response.status_code==200
