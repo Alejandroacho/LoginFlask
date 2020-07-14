e63bb0732b1382ad58a3290b9e6cf99569a7dba4
Alejandro <alejandroacho@hotmail.com>
2020-07-14 20:04:00 +0200

    Tests for index + Front modified

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,25 @@
 from main import webserver
+import flask
 import pytest
  
 @pytest.fixture
 def client():
     app = webserver()
     return app.test_client()    
- 
-def test_index_pages_works(client):
+
+#==================INDEX ROUTE TESTS=====================# 
+def test_if_index_route_works (client):
     response = client.get('/')
     assert response.status_code==200
 
-#def test_index_pages_works(client):
-#    response = client.get('mailto:contact@neveralone.com')
-#    assert response.status_code==200
+def test_if_index_has_its_key_button (client):
+    response = client.get('/')
+    assert b'<a href="#about" class="get-started-btn scrollto">Get Started</a>' in response.data
+
+def test_if_index_has_emailto_link (client):
+    response = client.get('/')
+    assert b'<a href="mailto:contact@neveralone.com">contact@neveralone.com</a>' in response.data
+
+def test_if_index_has_callto_link (client):
+    response = client.get('/')
+    assert b'<a href="tel:+34640518009">+34 640 51 80 09</a>' in response.data
